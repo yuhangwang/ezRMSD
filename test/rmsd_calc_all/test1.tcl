f7d4f9ab1ccd4d19a523ca0cd5df2d298f6cd257
yuhangwang <stevenaura@outlook.com>
2018-04-20 10:40:46 -0500

    update tests for ::rmsd::calc::all

--- test/rmsd_calc_all/test1.tcl
+++ test/rmsd_calc_all/test1.tcl
@@ -5,10 +5,17 @@ package require dict_compare_list
 proc test {} {
     set id1 [::vmd::io load "input/p1.pdb"]
     set id2 [::vmd::io load "input/p2.pdb"]
-    set seltxt "all"
+    
+    set seltxt1 "all"
+    set seltxt2 "all"
+    set common [::rmsd::common_part $id1 $seltxt1 $id2 $seltxt2]
+    set seltxt [::rmsd::seltxt $common]
+    set ref    [::atomselect $id1 $seltxt]
+    set target [::atomselect $id2 $seltxt]
+    
     set expected 0.0
 
-    set answer [rmsd calc all $id1 $seltxt $id2 $seltxt]
+    set answer [rmsd calc all $ref $target $common]
     
     if {$answer == $expected} {
         puts ">>> PASS!"
--- test/rmsd_calc_all/test3.tcl
+++ test/rmsd_calc_all/test3.tcl
@@ -6,11 +6,18 @@ package require dict_compare_list
 proc test {} {
     set id1 [::vmd::io load "input/p3.pdb"]
     set id2 [::vmd::io load "input/p4.pdb"]
-    set seltxt "all"
+    
+    set seltxt1 "all"
+    set seltxt2 "all"
+    set common [::rmsd::common_part $id1 $seltxt1 $id2 $seltxt2]
+    set seltxt [::rmsd::seltxt $common]
+    set ref    [::atomselect $id1 $seltxt]
+    set target [::atomselect $id2 $seltxt]
+
     set expected [expr sqrt(0.5)]
     set tol 1e-5
 
-    set answer [rmsd calc all $id1 $seltxt $id2 $seltxt false]
+    set answer [rmsd calc all $ref $target $common]
     
     if {[expr $answer - $expected] < $tol} {
         puts ">>> PASS!"
